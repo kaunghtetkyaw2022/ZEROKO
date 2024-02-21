c729d496860e69cb73d9a9c418dbc583cbd7d34d
kaunghtetkyaw2022 <140709011+kaunghtetkyaw2022@users.noreply.github.com>
2024-02-22 00:49:29 +0630

    Add files via upload

---
+++ Tani.py
@@ -0,0 +1,25 @@
+import os, platform
+ 
+try:
+ 
+    import requests
+ 
+except:
+ 
+    os.system('pip install requests')
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == '64bit':
+ 
+    from Tanu import iAmMain
+ 
+    iAmMain().iAmMenu()
+ 
+elif bit == '32bit':
+ 
+    from Tanu2 import iAmMain
+ 
+    iAmMain().iAmMenu()
